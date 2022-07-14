a37bb5c7755b17fcd5009b63be4cfa8f3781b112
TeddyTjoe1990 <her21059@byui.edu>
2022-07-14 12:35:30 +0700

    weather app was made

---
+++ webproject/weather/js/images.js
@@ -0,0 +1,4 @@
+const imageURL = 'https://openweathermap.org/img/wn/10d@2x.png';
+// let tempImage = document.getElementById('icon');
+
+document.getElementById("tempIcon").setAttribute('src', imageURL);
